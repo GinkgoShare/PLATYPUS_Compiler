72e15bdf71ee44319c23ad469eaaf5d900acf97b
Chris Elli <GinkgoShare@users.noreply.github.com>
2016-01-11 22:08:51 -0500

    Delete generate_exe.bat

--- src/generate_exe.bat
+++
@@ -1,5 +0,0 @@
-bcc32 -A -w platy.c buffer.c scanner.c stable.c parser.c stack.c list.c
-
-copy platy.exe c:\Users\cjwel\OneDrive\Projects\platypus_interpreter\test_files\shell\
-
-del *.obj *.res *.tds *.map
